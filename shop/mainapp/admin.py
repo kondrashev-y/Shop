649103f050e743265e39e0a31ff1ebf06218a31d
kondrashev-y <70172501+kondrashev-y@users.noreply.github.com>
2020-11-24 13:27:43 +0300

    Initial commit

---
+++ shop/mainapp/admin.py
@@ -0,0 +1,100 @@
+from PIL import Image
+
+from django.contrib import admin
+from django.forms import ModelChoiceField, ModelForm, ValidationError
+from django.utils.safestring import mark_safe
+
+from .models import *
+
+
+class CleanImage():
+
+    def clean_image(self):
+        image = self.cleaned_data['image']
+        img = Image.open(image)
+        min_height, min_width = Product.MIN_RESOLUTION
+        # max_height, max_width = Product.MAX_RESOLUTION
+        if image.size > Product.MAX_IMAGE_SIZE:
+            raise ValidationError('Размер изображение не должен превышать 3MB!')
+        if img.height < min_height or img.width < min_width:
+            raise ValidationError('Разрешение изображения меньше минимального!')
+        # if img.height > max_height or img.width > max_width:
+        #     raise ValidationError('Разрешение изображения выше максимального!')
+        return image
+
+
+class SmartphoneAdminForm(ModelForm, CleanImage):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        instance = kwargs.get('instance')
+        if instance and not instance.sd:  # if instance and not instance.sd:
+            self.fields['sd_volume_max'].widget.attrs.update({
+                'readonly': True, 'style': 'background: lightgray;'
+            })
+
+        self.fields['image'].help_text = mark_safe(
+            """<span style="color:red; font-size: 14px;">Изображение с расширением больше {}x{} будет обрезано!</span>
+            """.format(
+                *Product.MAX_RESOLUTION
+            )
+        )
+
+    def clean(self):
+        if not self.cleaned_data['sd']:
+            self.cleaned_data['sd_volume_max'] = None
+        return self.cleaned_data
+
+
+class NotebookAdminForm(ModelForm, CleanImage):  # делаем форму неактивной если не выбрана карта sd
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['image'].help_text = mark_safe(
+            """<span style="color:red; font-size: 14px;">Изображение с расширением больше {}x{} будет обрезано!</span>
+            """.format(
+                *Product.MAX_RESOLUTION
+            )
+        )
+
+    # def clean_image(self):
+    #     image = self.cleaned_data['image']
+    #     img = Image.open(image)
+    #     min_height, min_width = Product.MIN_RESOLUTION
+    #     # max_height, max_width = Product.MAX_RESOLUTION
+    #     if image.size > Product.MAX_IMAGE_SIZE:
+    #         raise ValidationError('Размер изображение не должен превышать 3MB!')
+    #     if img.height < min_height or img.width < min_width:
+    #         raise ValidationError('Разрешение изображения меньше минимального!')
+    #     # if img.height > max_height or img.width > max_width:
+    #     #     raise ValidationError('Разрешение изображения выше максимального!')
+    #     return image
+
+
+class NotebookAdmin(admin.ModelAdmin):
+
+    form = NotebookAdminForm
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return ModelChoiceField(Category.objects.filter(slug='notebooks'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+class SmartphoneAdmin(admin.ModelAdmin):
+
+    change_form_template = 'admin.html'
+    form = SmartphoneAdminForm
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return ModelChoiceField(Category.objects.filter(slug='smartphones'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+admin.site.register(Category)
+admin.site.register(Notebook, NotebookAdmin)
+admin.site.register(Smartphone, SmartphoneAdmin)
+admin.site.register(CartProduct)
+admin.site.register(Cart)
+admin.site.register(Customer)
